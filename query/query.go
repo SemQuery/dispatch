ddc1cd9a0ce2cbc0c05f10aabf3d5758b126bcdd
Kevin <kevtehpro35@gmail.com>
2015-12-07 22:29:41 -0500

    Added Query mode to dispatch

--- query/query.go
+++ query/query.go
@@ -5,6 +5,8 @@ import (
     "github.com/semquery/dispatch/common"
 
     "bufio"
+    "strings"
+    "strconv"
     "net/url"
     "os/exec"
     "net/http"
@@ -32,14 +34,27 @@ func Start() {
         read, _ := quer.StdoutPipe()
         scanner := bufio.NewScanner(read)
 
-        for scanner.Scan() {
-        }
-
         p := common.Packet {
             Action: "results",
             Payload: map[string]interface{} {},
         }
 
+        var results []map[string]interface{}
+        for scanner.Scan() {
+            parts := strings.Split(scanner.Text(), ",")
+            if len(parts) == 1 {
+                p.Payload["files"] = parts[0]
+                count, _          := strconv.Atoi(parts[0])
+                results            = make([]map[string]interface{}, count)
+            } else {
+                results = append(results, map[string]interface{} {
+                    "src":   parts[0],
+                    "start": parts[1],
+                    "end":   parts[2],
+                })
+            }
+        }
+        p.Payload["results"] = results
         return p.Json()
     })
     m.RunOnAddr("localhost:3001")
